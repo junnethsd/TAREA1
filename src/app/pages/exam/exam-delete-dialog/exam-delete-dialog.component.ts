f2919c2740ead9533ec62707e1979c9254ebf2fa
jperezr <jperezr@mininter.gob.pe>
2024-11-08 16:34:13 -0500

    tarea angular

--- src/app/pages/exam/exam-delete-dialog/exam-delete-dialog.component.ts
+++ src/app/pages/exam/exam-delete-dialog/exam-delete-dialog.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../../material/material.module';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-exam-delete-dialog',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './exam-delete-dialog.component.html',
   styleUrl: './exam-delete-dialog.component.css'
 })
 export class ExamDeleteDialogComponent {
+  constructor(private dialogRef: MatDialogRef<ExamDeleteDialogComponent>) {}
+  onConfirm(): void {
+    this.dialogRef.close(true); // Cierra el diálogo y emite true
+  }
 
+  onCancel(): void {
+    this.dialogRef.close(false); // Cierra el diálogo y emite false
+  }
 }
